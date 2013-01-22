af0adb8384cde02e907f02d57d8322fc686c34ef
Shane R. Spencer <spencersr@buckaroobanzai.(none)>
2013-01-22 09:07:31 -0900

    added nojekyll

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,modul:0,indic:0,content:0,tabl:0,indieinfotech:0,page:0},objtypes:{},titles:["Welcome to IndieInfoTech!"],objnames:{},filenames:["index"]})
